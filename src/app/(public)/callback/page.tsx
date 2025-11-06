1a81703ccb2cef73e0241d3288d3ec475e9b782c
OhChangEun <dwc07109@gmail.com>
2025-11-06 14:28:19 +0900

    #76 fix(production): 리다이렉트 url, 클라이언트 id를 로컬용으로 수정

--- src/app/(public)/callback/page.tsx
+++ src/app/(public)/callback/page.tsx
@@ -8,8 +8,8 @@ import { useQuery } from '@tanstack/react-query';
 import { getUserInfo } from './callback.api';
 import { useAuthStore } from '@/store/authStore';
 
-// const REDIRECT_URI = 'http://localhost:3000/callback'; // 배포용
-const REDIRECT_URI = 'https://everp.co.kr/callback'; // 서버용
+const REDIRECT_URI = 'http://localhost:3000/callback'; // 배포용
+// const REDIRECT_URI = 'https://everp.co.kr/callback'; // 서버용
 
 function saveAccessToken(accessToken: string, expiresIn: number) {
   localStorage.setItem('access_token', accessToken);
@@ -73,8 +73,8 @@ export default function CallbackPage() {
 
         const body = new URLSearchParams({
           grant_type: 'authorization_code',
-          // client_id: 'everp-spa', // 로컬용
-          client_id: 'everp', // 배포용
+          client_id: 'everp-spa', // 로컬용
+          // client_id: 'everp', // 배포용
           redirect_uri: REDIRECT_URI,
           code,
           code_verifier: verifier,
@@ -84,7 +84,7 @@ export default function CallbackPage() {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             // 동적으로 키 생성
-            Authorization: makeBasicAuthHeader('everp', 'super-secret'),
+            // Authorization: makeBasicAuthHeader('everp', 'super-secret'),
           },
         });
 
