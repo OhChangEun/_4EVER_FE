b77522b85fcb274722cb0e76b353951d4e521287
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-11-06 11:37:00 +0900

    #auth fix(auth): axiosInsance 토큰 불러오는 방식 수정

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
+
 axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem('access_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== 'undefined') {
+    const token = window.localStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
+
   return config;
 });
 
