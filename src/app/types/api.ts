aa4c4293319ad628f66d0664fe09f11ceebcaeb3
OhChangEun <dwc07109@gmail.com>
2025-11-08 01:45:27 +0900

    #76 fix(production): base url 임시 배포 주소 원상복구

--- src/app/types/api.ts
+++ src/app/types/api.ts
@@ -1,6 +1,6 @@
 // 공통 API Base URL
-// export const API_BASE_URL = 'https://api.everp.co.kr/api';
-export const API_BASE_URL = 'https://b5bd9b62ef90.ngrok-free.app/api';
+export const API_BASE_URL = 'https://api.everp.co.kr/api';
+// export const API_BASE_URL = 'https://b5bd9b62ef90.ngrok-free.app/api';
 
 export const SALES_BASE_PATH = `${API_BASE_URL}/business/sd`;
 export const FINANCE_BASE_PATH = `${API_BASE_URL}/business/fcm`;
