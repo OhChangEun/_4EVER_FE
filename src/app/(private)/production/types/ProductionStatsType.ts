208c50e551a61dd60ab21e5dbca5fa393ecff750
OhChangEun <dwc07109@gmail.com>
2025-11-09 17:44:10 +0900

    #127 refactor(production): 생산 관리 통계 카드 dto 변경에 따른 수정

--- src/app/(private)/production/types/ProductionStatsType.ts
+++ src/app/(private)/production/types/ProductionStatsType.ts
@@ -2,7 +2,7 @@ import { Stat, StatResponse } from '@/app/types/StatType';
 
 // 생산 지표 api 응답 데이터 구조
 export interface ProductionStat {
-  production_in_progress: Stat;
+  production_in: Stat;
   production_completed: Stat;
   bom_count: Stat;
 }
